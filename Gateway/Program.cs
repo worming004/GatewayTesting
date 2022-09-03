56bc5668fb42dee1823a991658602b5786a89e2b
Mathieu Scolas <worming@hotmail.be>
2022-09-03 11:10:16 +0200

    use httpclient

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,3 +1,5 @@
+using Gateway.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient<RepeatController>(opt => opt.BaseAddress = new Uri("http://localhost:3000/"));
 
 var app = builder.Build();
 
